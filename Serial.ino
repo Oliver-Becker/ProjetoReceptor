adf73bd23ba488e0ae619abcb5632b80aac7ba7b
Óliver S Becker <oliverbecker@usp.br>
2019-04-29 04:44:19 -0300

    Tentativa de implementar o handshake :D

--- Serial.ino
+++ Serial.ino
@@ -1,5 +1,7 @@
 #define PINO_RX 13
 #define PINO_TX 13
+#define PINO_HANDSHAKE_IN 12
+#define PINO_HANDSHAKE_OUT 11
 #define BAUD_RATE 1
 #define HALF_BAUD 1000/(2*BAUD_RATE)
 #define WORD_LENGHT 8
@@ -12,6 +14,7 @@ char start;
 char dados;
 char i;
 char leitura;
+char handshake;
 
 // Calcula bit de paridade - Par ou impar
 bool bitParidade(char dado, char paridade) {
@@ -34,7 +37,8 @@ ISR(TIMER1_COMPA_vect) {
   leitura = digitalRead(PINO_RX); // Lê do input
   Serial.print("Entrada = ");
   Serial.println(leitura == HIGH);
-  if (start == LOW && i++ < (WORD_LENGHT + 1)) {    
+  
+  if (i++ < (WORD_LENGHT + 1)) {    // Se ainda estiver lendo a palavra
     if (i == WORD_LENGHT + 1) { // Caso seja o bit de paridade
       bitParidade(dados, leitura);
     } else {  // Senão, adiciona o bit aos dados
@@ -42,15 +46,17 @@ ISR(TIMER1_COMPA_vect) {
       if (leitura == HIGH)
         dados |= 1;
     }
-  } else {
-    start = leitura;    
+  } else {          // Já acabou a palavra, então para o temporizador
+    start = HIGH;    
     i = 0;
+    paraTemporizador();
   }
 }
 
 // Executada uma vez quando o Arduino reseta
 void setup() {
   start = HIGH;
+  handshake = LOW;
   i = 0;
   //desabilita interrupcoes
   noInterrupts();
@@ -58,14 +64,30 @@ void setup() {
   Serial.begin(9600);
   // Inicializa TX ou RX
   pinMode(PINO_RX, INPUT);
+  pinMode(PINO_HANDSHAKE_IN, INPUT);
+  pinMode(PINO_HANDSHAKE_OUT, OUTPUT);
   // Configura timer
   configuraTemporizador(BAUD_RATE);
-  iniciaTemporizador();
   // habilita interrupcoes
   interrupts();
 }
 
 // O loop() eh executado continuamente (como um while(true))
 void loop ( ) {
-  //>>>> Codigo Aqui <<<<
+  leitura = digitalRead(PINO_HANDSHAKE_IN); // Lê do input
+  if (leitura == HIGH && handshake == LOW) {    // Iniciaram o handshake mas ainda não respondi
+    handshake = HIGH;
+    digitalWrite(PINO_HANDSHAKE_OUT, handshake);  // Seta o pino de resposta pra HIGH
+  } else if (leitura == LOW && handshake == HIGH) {  // Finalizaram o handshake 
+    handshake = LOW;
+    digitalWrite(PINO_HANDSHAKE_OUT, handshake);  // Seta o pino de resposta pra LOW
+  }
+  if (handshake == HIGH) {
+    leitura = digitalRead(PINO_RX);
+    if (leitura == LOW && start == HIGH) {   // Enviaram sinal de start bit
+      delay(HALF_BAUD);
+      start = LOW;
+      iniciaTemporizador();
+    }
+  }
 }
